05daef9be5ed89e998414ae1dd4c4908cda92858
acmeyer <acmeyer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-11 16:20:06 +0000

    update plugin to 2.0
    git-svn-id: https://plugins.svn.wordpress.org/privy-website-widget/trunk@1158078 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- privy-marketing-widget.js
+++ privy-marketing-widget.js
@@ -1,5 +1,5 @@
 //PRIVY WIDGET CODE
 var _d_site = _d_site || PrivyWebsiteWidgetParams.account_identifier; (function() {
 var script = document.createElement('script'); script.type = 'text/javascript'; script.async = true;
-script.src = document.location.protocol + '//beta.privy.com/assets/widget.js';
+script.src = document.location.protocol + '//widget.privy.com/assets/widget.js';
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(script, s); })();
